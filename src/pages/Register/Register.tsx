5759346113f83d1c132ff71c60f3e2f6a5a2bd89
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-09-22 16:01:53 +0530

    firebase auth + online logic done

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,5 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../utils/firebase";
+import { ref, set } from "firebase/database";
+import { useNavigate } from "react-router-dom";
+
+interface UserData {
+  name: string;
+  email: string;
+  password: string;
+  error: string | null;
+  loading: boolean;
+}
 
 export default function Register() {
-  return <div>Register</div>;
+  const [data, setData] = useState<UserData>({
+    name: "",
+    email: "",
+    password: "",
+    error: null,
+    loading: false,
+  });
+  const navigate = useNavigate();
+
+  const { name, email, password, error, loading }: UserData = data;
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setData({ ...data, error: null, loading: true });
+    if (!name || !email || !password) {
+      setData({ ...data, error: "All fields are required" });
+    }
+    console.log(data);
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      set(ref(db, "users/" + result.user.uid), {
+        uid: result.user.uid,
+        name,
+        email,
+        isOnline: true,
+      });
+      setData({
+        name: "",
+        email: "",
+        password: "",
+        error: null,
+        loading: false,
+      });
+      navigate("/");
+    } catch (err) {
+      setData({ ...data, error: err.message, loading: false });
+    }
+  };
+  return (
+    <div>
+      <div className="flex flex-col items-center min-h-screen  sm:justify-center sm:pt-0 bg-gray-50">
+        <div>
+          <a href="/">
+            <h3 className="text-4xl font-bold text-purple-600">Logo</h3>
+          </a>
+        </div>
+        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
+          <form>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Name
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={handleChange}
+                  className="block w-full mt-1 border-violet-600 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Email
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                  className="block w-full mt-1 text-gray-900 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Password
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                  className="block w-full mt-1 text-black border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            {error ? <p className=" text-red-600">{error}</p> : null}
+            <div className="flex items-center justify-end mt-4">
+              <a
+                className="text-sm text-gray-600 underline hover:text-gray-900"
+                href="#"
+              >
+                Already registered?
+              </a>
+              <button
+                type="submit"
+                disabled={loading}
+                onClick={handleSubmit}
+                className="inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900 false"
+              >
+                {loading ? "Creating ..." : "Register"}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
